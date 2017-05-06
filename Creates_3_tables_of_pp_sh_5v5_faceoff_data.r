241703ae80c563a13617e94b77d8eb59c6d6ec45
Charlie Chafin <chafincharlie11@gmail.com>
2017-05-05 22:16:39 -0500

    Update Creates_3_tables_of_pp_sh_5v5_faceoff_data.r
    used paste() to create the proper column titles. Removed like 50 lines of code. I feel proud.

--- Creates_3_tables_of_pp_sh_5v5_faceoff_data.r
+++ Creates_3_tables_of_pp_sh_5v5_faceoff_data.r
@@ -1,8 +1,10 @@
+
 #install.packages("rvest")
 library("rvest")
 library(data.table)
 library(stringr)
 library(dplyr)
+
 z=1
 situations = c('sh','pp','5v5')
 for (i in situations){
@@ -15,55 +17,15 @@ for (i in situations){
     html_table(fill=TRUE)
     population[[1]]$year= x+1
     population[[1]][1] = NULL
+    a=situations[z]
     assign(paste0("X",situations[z], x+1), population[[1]])
+    setnames(population[[1]], old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
+             new =c(paste(a,"FO%"),paste(a,"Games"),paste(a,"FO per game"),
+                    paste(a,"FW per game"), paste(a,"FW"), paste(a,"FO")))
     x=x+1
   }
   z=z+1    
 }
-
-setnames(X5v52014, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("5v5 FO%","5v5 Games","5v5 FO per game",
-                "5v5 FW per game", "5v5 FW", "5v5 FO"))
-setnames(X5v52015, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-                   new =c("5v5 FO%","5v5 Games","5v5 FO per game",
-                        "5v5 FW per game", "5v5 FW", "5v5 FO"))
-
-
-setnames(X5v52016, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("5v5 FO%","5v5 Games","5v5 FO per game",
-                "5v5 FW per game", "5v5 FW", "5v5 FO"))
-setnames(X5v52017, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("5v5 FO%","5v5 Games","5v5 FO per game",
-                "5v5 FW per game", "5v5 FW", "5v5 FO"))
-
-setnames(Xpp2014, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("pp FO%","pp Games","pp FO per game",
-                "pp FW per game", "pp FW", "pp FO"))
-setnames(Xpp2015, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("pp FO%","pp Games","pp FO per game",
-                "pp FW per game", "pp FW", "pp FO"))
-
-setnames(Xpp2016, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("pp FO%","pp Games","pp FO per game",
-                "pp FW per game", "pp FW", "pp FO"))
-setnames(Xpp2017, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("pp FO%","pp Games","pp FO per game",
-                "pp FW per game", "pp FW", "pp FO"))
-setnames(Xsh2014, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("sh FO%","sh Games","sh FO per game",
-                "sh FW per game", "sh FW", "sh FO"))
-
-setnames(Xsh2015, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("sh FO%","sh Games","sh FO per game",
-                "sh FW per game", "sh FW", "sh FO"))
-setnames(Xsh2016, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("sh FO%","sh Games","sh FO per game",
-                "sh FW per game", "sh FW", "sh FO"))
-setnames(Xsh2017, old= c("FO%","Games","FO per game","FW per game", "FW", "FO"),
-         new =c("sh FO%","sh Games","sh FO per game",
-                "sh FW per game", "sh FW", "sh FO"))
-
-
 
 shorthanded = rbind(Xsh2014,Xsh2015,Xsh2016,Xsh2017)
 powerplay = rbind(Xpp2014,Xpp2015,Xpp2016,Xpp2017)
