75d060df7c54650945ada9522e7d0950a0e11325
Daniel Hoffmann <daniel.hoffmann@maritz.com>
2017-05-23 10:41:37 -0500

    puckalytics scraper for machine learning

--- puckalytics_scraper.R
+++ puckalytics_scraper.R
@@ -6,15 +6,12 @@ library(dplyr)
 library(dtplyr)
 
 z=1
-situations = c('5v5','5v5home','5v5road','5v5close_home', '5v5close_road', '5v5close','5v5tied','5v5tied_home','5v5tied_road',
-               '5v5leading','5v5leading_home','5v5leading_road','5v5trailing','5v5trailing_home','5v5trailing_road',
-               '5v5up1','5v5up2','5v5down1','5v5down2','4v4','threeonthree','5v5firstperiod','5v5secondperiod','5v5thirdperiod',
-               'all','5v4','4v5','PP','SH')
+situations = c('5v5','5v5home','5v5road')
 #seasons= c('201617','201516','201415','201314','201213')
 for (i in situations){
   x = 2013
   y=14
-  for(i in 1:4){
+  for(i in 1:3){
     url <- sprintf("http://stats.hockeyanalysis.com/teamstats.php?db=%s%s&sit=%s&disp=1&sortdir=DESC&sort=GFPCT",x,y, situations[z])
     population <- url %>%
       read_html() %>%
@@ -32,5 +29,3 @@ for (i in situations){
   }
   z=z+1    
 }
-fours= rbind(`4v42014`,`4v42015`,`4v42016`,`4v42017`)
-rm(`4v42014`,`4v42015`,`4v42016`,`4v42017`)
